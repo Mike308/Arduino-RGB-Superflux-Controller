a4777f564523a3fd3f7111b38862a9868c716507
Majkel44 <m.werner9205@gmail.com>
2016-12-17 14:51:05 +0100

    - init commit

---
+++ RGB_PWM_LAMP/RGB_PWM_LAMP.ino
@@ -0,0 +1,20 @@
+/*
+ * RGB_PWM_LAMP.ino
+ *
+ * Created: 12/17/2016 2:48:00 PM
+ * Author: Mike
+ */ 
+
+void setup()
+{
+
+	  /* add setup code here, setup code runs once when the processor starts */
+
+}
+
+void loop()
+{
+
+	  /* add main program code here, this code starts again each time it ends */
+
+}
---
+++ RGB_PWM_LAMP/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * RGB_PWM_LAMP.cpp
+ *
+ * Created: 2016-12-17 14:48:01
+ * Author : Mike
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
