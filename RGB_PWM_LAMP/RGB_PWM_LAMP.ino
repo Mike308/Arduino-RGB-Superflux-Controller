26997e8198f5385fa98f288e42cb93b4192b7ce9
Majkel44 <m.werner9205@gmail.com>
2016-12-17 17:31:42 +0100

    - added function for hsv_to_rgb conversion

--- RGB_PWM_LAMP/RGB_PWM_LAMP.ino
+++ RGB_PWM_LAMP/RGB_PWM_LAMP.ino
@@ -10,6 +10,13 @@
 #define G 6
 #define B 3
 
+
+
+void HSVtoRGB(float h,float s,float v);
+
+
+
+
 void setup()
 {
 
@@ -17,6 +24,7 @@ void setup()
 	  pinMode(R,OUTPUT);
 	  pinMode(G,OUTPUT);
 	  pinMode(B,OUTPUT);
+	  Serial.begin(9600);
 
 }
 
@@ -24,8 +32,86 @@ void loop()
 {
 
 	  /* add main program code here, this code starts again each time it ends */
-	  analogWrite(B,255);
-	  analogWrite(R,128);
-	  analogWrite(G,120);
+	HSVtoRGB(221,100,100);
+	  
 
 }
+
+
+/** MODIFIED FUNCTION FROM ws2812b (http://mikrocontroller.bplaced.net/wordpress/?page_id=3665)**/
+
+
+void HSVtoRGB(float h,float s,float v)
+{
+	uint8_t diff;
+	
+	byte red,green,blue;
+
+	
+	if(h>359) h=359;
+	if(s>100) s=100;
+	if(v>100) v=100;
+
+	if(h < 61) {
+			red = 255;
+			green = (425 * h) / 100;
+			blue = 0;
+		}else if(h < 121){
+			red = 255 - ((425 * (h-60))/100);
+			green = 255;
+			blue = 0;
+		}else if(h < 181){
+			red = 0;
+			green = 255;
+			blue = (425 * (h-120))/100;
+		}else if(h < 241){
+			red = 0;
+			green = 255 - ((425 * (h-180))/100);
+			blue = 255;
+		}else if(h < 301){
+			red = (425 * (h-240))/100;
+			green = 0;
+			blue = 255;
+		}else {
+			red = 255;
+			green = 0;
+			blue = 255 - ((425 * (h-300))/100);
+	}
+
+	s = 100 - s;
+	diff = ((255 - red) * s)/100;
+	red =	red + diff;
+	diff = ((255 - green) * s)/100;
+	green = green + diff;
+	diff = ((255 - blue) * s)/100;
+	blue = blue + diff;
+
+	red = (red * v)/100;
+	green = (green * v)/100;
+	blue = (blue * v)/100;
+	
+	analogWrite(R,red);
+	analogWrite(G,green);
+	analogWrite(B,blue);
+	Serial.print(red);
+	Serial.print(",");
+	Serial.print(green);
+	Serial.print(",");
+	Serial.println(blue);
+}
+
+
+
+
+
+
+		
+		
+		
+	
+	
+	 
+	 
+	
+	
+
